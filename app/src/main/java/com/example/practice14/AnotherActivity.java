70fca5a3463e744d7b2dd748677a35490a6615ff
zenghuazhe <570421628@qq.com>
2016-09-17 20:33:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/practice14/AnotherActivity.java
@@ -0,0 +1,14 @@
+package com.example.practice14;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+
+    }
+}
